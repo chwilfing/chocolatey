7bd9f815de31adeb8be4ab1bc1c80776d497ced8
Christoph Wilfing <christoph.wilfing@atwork.at>
2018-12-11 08:22:32 +0100

    Update Version 1.41.51 and fixed issue with download url

--- azure-information-protection-client/tools/chocolateyinstall.ps1
+++ azure-information-protection-client/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AZInfoProtection_MSI_for_central_deployment.msi'
-              
+$url      = 'https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AzInfoProtection_for_central_deployment.msi'
+             
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName  = 'azure-information-protection-client'
 
-  checksum      = '3A29B23D319D0CE90559A67359A65A56E74C2CD433774D14B24BEE42AFB0D148'
+  checksum      = '2F4C63AB87A8A6B08AACE883C02B9CE0D5715B83E5320EEB42A5E2FA4EB9AA90'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
