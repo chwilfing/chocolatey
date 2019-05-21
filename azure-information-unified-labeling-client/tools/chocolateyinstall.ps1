005e5943c07a58e0a78269b18dbc51f7a9ffb72a
Christoph Wilfing <christoph.wilfing@atwork.at>
2019-05-21 21:53:32 +0200

    updated version to 2.0.779.0

---
+++ azure-information-unified-labeling-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AzInfoProtection_ul_MSI_for_central_deployment.msi'
+             
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'MSI'
+    url            = $url
+
+    softwareName   = 'azure-information-protection-unified-labeling-client'
+
+    checksum       = 'EC3CAE8DE84BDA66CB6257CCC06D933892E0DC5CFC17E078656553422499BD8E'
+  
+    checksumType   = 'sha256'
+
+    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
