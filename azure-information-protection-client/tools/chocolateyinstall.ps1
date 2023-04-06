034c8060445621838466f3cb8b70a3b1f10496fe
Christoph Wilfing <christoph.wilfing@atwork.at>
2023-04-06 18:37:26 +0200

    bumped up the version to 2.15.33.0

--- azure-information-protection-client/tools/chocolateyinstall.ps1
+++ azure-information-protection-client/tools/chocolateyinstall.ps1
@@ -1,24 +1,24 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-$packageName= $env:ChocolateyPackageName
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AzInfoProtection_for_central_deployment.msi'
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://download.microsoft.com/download/4/9/1/491251F7-46BA-46EC-B2B5-099155DD3C27/AzInfoProtection_UL.msi'
              
              
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url           = $url
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url            = $url
 
-  softwareName  = 'azure-information-protection-client'
+  softwareName   = 'azure-information-protection-client'
 
-  checksum      = '09419CA6A3565F0AF2504B60FBB8E20A00CB852A14F3FE11BF73058029158B25'
-  checksumType  = 'sha256'
+  checksum       = '97A35D19F43ADC98938238568A6F3BEF9830C32F8C5C1FF4E3C1B3AD26FB7204'
+  checksumType   = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
